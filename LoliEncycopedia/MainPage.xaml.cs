b106791cdbbbe1e9e8857d0c0bb1dc187d6045a0
Hammerek <anakin165@wp.pl>
2016-05-05 16:07:57 +0200

    Initial commit.

---
+++ LoliEncycopedia/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using LoliEncycopedia;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LoliEncycopedia
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            arigato.ItemsSource = Scenarios; 
+        }
+        private static List<Scenario> Scenarios { get; } = new List<Scenario>
+        {
+            new Scenario() {ClassType = null, Icon = "aaaaaa", Title = "Monster" }
+
+        };
+
+
+    }
+    public class ImageBindingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var s = value as Scenario;
+            return new Uri("ms-appx:///Assets/Loli Pictures" + s.Icon + ".png");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+
+}
---
+++ LoliEncycopedia/Scenario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoliEncycopedia
+{
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+        public string Icon { get; set; }
+    }
+}
